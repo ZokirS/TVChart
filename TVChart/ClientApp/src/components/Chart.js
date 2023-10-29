2bba4bd5c3558d8f7f30041a6c9b53ded08f345a
Zokir Sobirjonov <zokir5857@gmail.com>
2023-10-29 22:56:14 +0500

    changes for both charts

--- TVChart/ClientApp/src/components/Chart.js
+++ TVChart/ClientApp/src/components/Chart.js
@@ -1,76 +1,79 @@
 import * as React from 'react';
-import { createChart, CrosshairMode, LineStyle } from 'lightweight-charts';
+import {
+    createChart,
+    CrosshairMode,
+    LineStyle
+} from 'lightweight-charts';
 import moment from 'moment';
 
 
 export class Chart extends React.Component {
-    
+
     static displayName = Chart.name;
 
     constructor(props) {
-      super(props);
-      this.chart = null;
-      this.state = { candles: [], loading: true };
+        super(props);
+        this.chart = null;
+        this.state = {
+            candles: [],
+            loading: true
+        };
     }
 
     componentDidMount() {
         this.populateChartData();
     }
 
-   
-    
-    static renderCandlesChart(candles){
+
+
+    static renderCandlesChart(candles) {
+        var chartEl = document.getElementById('chart');
+        var axisEl = document.getElementById('axis');
         const chartOptions = {
             width: window.width,
-            height: 500,
+            height: 250,
             crosshair: {
-                // Change mode from default 'magnet' to 'normal'.
-                // Allows the crosshair to move freely without snapping to datapoints
-                mode: CrosshairMode.Normal,
-        
-                // Vertical crosshair line (showing Date in Label)
                 vertLine: {
                     width: 8,
                     color: '#C3BCDB44',
                     style: LineStyle.Solid,
                     labelBackgroundColor: '#9B7DFF',
+                    visible: true,
+                    labelVisible: true
                 },
-        
+                mode: CrosshairMode.Magnet,
                 // Horizontal crosshair line (showing Price in Label)
                 horzLine: {
                     color: '#9B7DFF',
                     labelBackgroundColor: '#9B7DFF',
+                    visible: true,
+                    labelVisible: true
                 },
             },
             layout: {
-                background: { type: 'solid', color: 'white' } ,
+                background: {
+                    type: 'solid',
+                    color: 'white'
+                },
                 textColor: 'black'
-              },
-              timeScale: {
-                visible: true,
+            },
+            timeScale: {
                 timeVisible: true,
                 secondsVisible: true,
             }
         }
-        this.chart = createChart('chartContainer', chartOptions);
+        var chart = createChart(chartEl, chartOptions);
+        var axis = createChart(axisEl, chartOptions);
 
-        const barSeries = this.chart.addCandlestickSeries();
-        const rsiLine = this.chart.addLineSeries({
-            title: 'second',
-            priceFormat: {
-                minMove: 1,
-                precision: 0,
-            },
-            color: '#ff0000',
-            pane: 1
-        });
+        var barSeries = chart.addCandlestickSeries();
+        var lineSeries = axis.addAreaSeries();
         let candleData = [];
         let rsiData = [];
         candles.map(candle => {
             let time = moment(candle.openTime).utcOffset(0, true).valueOf() / 1000;
             candleData.push({
-                time : time,
-                open : candle.open,
+                time: time,
+                open: candle.open,
                 high: candle.high,
                 low: candle.low,
                 close: candle.close
@@ -81,32 +84,115 @@ export class Chart extends React.Component {
             })
         })
 
-        // set the data
+
         barSeries.setData(candleData);
-        rsiLine.setData(generateLineData(0, 100, 20));
+        lineSeries.setData(rsiData);
+
+        let isCrossHairMoving = false;
+        chart.subscribeCrosshairMove(param => {
+            if (!param.point) return;
+            if (!param.time) return;
+            if (isCrossHairMoving) return;
+
+            isCrossHairMoving = true;
+            axis.setCrosshairPosition(param.point, param.time, lineSeries);
+            isCrossHairMoving = false;
+        });
+
+        axis.subscribeCrosshairMove(param => {
+            if (!param.point) return;
+            if (!param.time) return;
+            if (isCrossHairMoving) return;
+
+            isCrossHairMoving = true;
+            chart.setCrosshairPosition(param.point, param.time, barSeries);
+            isCrossHairMoving = false;
+        });
+
+        var isChartActive = false;
+        var isAxisActive = false;
+        chartEl.addEventListener("mousemove", () => {
+            if (isChartActive) return;
+            isChartActive = true;
+            isAxisActive = false;
+            chart.applyOptions({
+                crosshair: {
+                    horzLine: {
+                        visible: true,
+                        labelVisible: true
+                    }
+                }
+            });
+            axis.applyOptions({
+                crosshair: {
+                    horzLine: {
+                        visible: false,
+                        labelVisible: false
+                    }
+                }
+            });
+        });
+
+        axisEl.addEventListener("mousemove", () => {
+            if (isAxisActive) return;
+            isAxisActive = true;
+            isChartActive = false;
+            axis.applyOptions({
+                crosshair: {
+                    horzLine: {
+                        visible: true,
+                        labelVisible: true
+                    }
+                }
+            });
+            chart.applyOptions({
+                crosshair: {
+                    horzLine: {
+                        visible: false,
+                        labelVisible: false
+                    }
+                }
+            });
+        });
+
+        chart.timeScale().subscribeVisibleLogicalRangeChange(range => {
+            axis.timeScale().setVisibleLogicalRange(range)
+        })
+        axis.timeScale().subscribeVisibleLogicalRangeChange(range => {
+            chart.timeScale().setVisibleLogicalRange(range)
+        })
     }
 
-	render() {
-        let contents = this.state.loading
-        ? <p><em>Loading...</em></p>
-        : Chart.renderCandlesChart(this.state.candles);
-		return (
-           
-			<div id='chartContainer'>{contents}</div>
-		);
-	}
-    async populateChartData(){
-        const response = await fetch("chart", { method: 'GET'});
-        const data = await response.json();
-        this.setState({candles: data, loading: false})
+    render() {
+        this.state.loading ?
+            <p> <em> Loading... </em></p>
+            : Chart.renderCandlesChart(this.state.candles);
+        return ( <div>
+                    <div id = "chart" ></div> 
+                    <div id = "axis" > </div>
+                </div>
+            );
     }
-     
+    async populateChartData() {
+        const response = await fetch("chart", {
+            method: 'GET'
+        });
+        const data = await response.json();
+        this.setState({
+            candles: data,
+            loading: false
+        })
+    }
+
 }
+
 function generateLineData(minValue, maxValue, maxDailyGainLoss = 1000) {
     var res = [];
     var time = new Date(Date.UTC(2018, 0, 1, 0, 0, 0, 0));
     for (var i = 0; i < 500; ++i) {
-        var previous = res.length > 0 ? res[res.length - 1] : { value: 0 };
+        var previous = res.length > 0 ? res[res.length - 1] : {
+            value: 0
+        };
         var newValue = previous.value + ((Math.random() * maxDailyGainLoss * 2) - maxDailyGainLoss);
 
         res.push({
